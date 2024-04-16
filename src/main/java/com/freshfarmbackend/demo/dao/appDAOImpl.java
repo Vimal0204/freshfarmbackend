77558772a862caa61f1d1f4f9cb620d6853019fc
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2024-04-16 20:03:14 +0530

    Update appDAOImpl.java

--- src/main/java/com/freshfarmbackend/demo/dao/appDAOImpl.java
+++ src/main/java/com/freshfarmbackend/demo/dao/appDAOImpl.java
@@ -274,7 +274,7 @@ public class appDAOImpl implements appDAO{
     @Override
     public String getFarmerMailByOrderId(int id) {
         Orders orders=entityManager.find(Orders.class,id);
-        return orders.getFarmers().getEmail();
+        // return orders.getFarmers().getEmail();
         // return orders.getStatus();
     }
 
