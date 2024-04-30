cb34b861337da8086011f3fc545bad58785a9d68
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2024-05-01 00:01:19 +0530

    Update FreshFarmRestController.java

--- src/main/java/com/freshfarmbackend/demo/rest/FreshFarmRestController.java
+++ src/main/java/com/freshfarmbackend/demo/rest/FreshFarmRestController.java
@@ -57,7 +57,7 @@ public class FreshFarmRestController {
 //        System.out.println(getProducts.get(0).getFarmer());
         return getProducts;
     }
-    @GetMapping("/getProductCategory/{category}")
+    @GetMapping("/categoryProduct/{category}")
     public List<Product> getProductByCategory(@PathVariable String category){
         List<Product> getProducts = appService.getProductByCategory(category);
 //        System.out.println(getProducts.get(0).getFarmer());
